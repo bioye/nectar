b4117c9979383189676c1a4eb51fc89cfcfec3a3
Abioye Bankole <bioyexx@yahoo.com>
2017-10-23 12:41:18 +0100

    Merged amir and medium to achieve registration, confirm and login.
    Code works locally.  Also includes mail functionality, password
    encryption, and password validation.

---
+++ nectarNew/src/main/java/ng/nectar/nectarNew/NectarNewApplication.java
@@ -0,0 +1,12 @@
+package ng.nectar.nectarNew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NectarNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NectarNewApplication.class, args);
+	}
+}
---
+++ nectarNew/src/test/java/ng/nectar/nectarNew/NectarNewApplicationTests.java
@@ -0,0 +1,16 @@
+package ng.nectar.nectarNew;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NectarNewApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
